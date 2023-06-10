94f538c45aea1b0510280706c8062993857e694b
Miguel Fernández Fernández <miguel.ffernandez@udc.es>
2023-06-10 11:55:33 +0000

    Proyecto Scrapy

---
+++ app/scrapy_app/billboard/billboard/spiders/billboard_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+class BillboardSpider(scrapy.Spider):
+    name = "billboard"
+    start_urls = ["https://www.billboard.com/c/music/music-news/"]
+
+    def parse(self, response):
+        titles = response.css('h3.c-title a::text').extract()
+        titles = [title.strip() for title in titles if title.strip()]
+        
+        for title in titles[:5]:
+            yield {
+                'title': title
+            }
